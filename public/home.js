aed1a2b3a3f3fa88ef867136572f993be9a2eb54
Claudio <imclaudfuen@gmail.com>
2018-11-04 18:52:39 -0500

    Updated Website

---
+++ public/home.js
@@ -0,0 +1,15 @@
+
+
+
+document.querySelector("#ceo-books").addEventListener('click', function(){
+    window.location.href = "/books";
+})
+
+document.querySelector("#ceo-directory").addEventListener('click', function(){
+    window.location.href = "/directory";
+})
+
+
+document.querySelector("#ceo-events").addEventListener('click', function(){
+    window.location.href = "/events";
+})
