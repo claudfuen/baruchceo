7914a069ef0109c3e48c91eb92beeb97f17980d0
Claudio <imclaudfuen@gmail.com>
2018-11-05 14:35:43 -0500

    Cleaned Up Routes

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'key4FeankqvbU5BHG'}).base('appzGAEJinOXICiPx');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://user:lll2j6raqI6sUpDX@baruchceo-wd33t.mongodb.net/BaruchCEO?retryWrites=true";
 
 
 app.use(express.static("public"));
@@ -23,13 +18,6 @@ app.get("/team", function(req,res){
 	res.render("team.ejs");
 })
 
-app.get("/signup", function(req,res){
-	res.render("signup");
-})
-
-app.get("/confirmation", function(req,res){
-	res.render("confirmation");
-})
 
 app.get("/directory", function(req, res) {
     res.render("directory");
@@ -44,48 +32,9 @@ app.get("/events", function(req, res) {
     res.render("events");
 });
 
-
-app.get("/books", function(req, res) {
-    res.render("books");
-});
-
-app.post("/signup", function(req, res){
-	var email = req.body.email;
-	var major = req.body.major;
-	var phoneNumber = req.body.phoneNumber;
-	var reason = req.body.reason;
-	var fullName = req.body.fullName;
-
-MongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	  var dbo = db.db("WEBSITE_DB");
-	  var myobj = req.body;
-	  dbo.collection("USERS").insertOne(myobj, function(err, res) {
-	    if (err) throw err;
-	    console.log("1 document inserted");
-	    db.close();
-	  });
-	});
-
-base('Applicants').create({
-  "major": major,
-  "email": email,
-  "phoneNumber": phoneNumber,
-  "reason": reason,
-  "fullName": fullName
-}, function(err, record) {
-    if (err) { console.error(err); return; }
-    console.log(record.getId());
-});
-
-res.redirect("/confirmation");
-});
-
-
 app.get("*", function(req, res) {
-    res.send("Sorry, page not found...");
+    res.redirect('/')
 });
-
 
 
 app.listen(PORT, function(){
