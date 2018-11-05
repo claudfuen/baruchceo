d31b3db8bebe762f899d9979b84e4423db23e315
Claudio <imclaudfuen@gmail.com>
2018-11-04 23:06:53 -0500

    First version finished

--- public/home.js
+++ public/home.js
@@ -17,3 +17,7 @@ document.querySelector("#ceo-events").addEventListener('click', function(){
 document.querySelector("#ceo-apparel").addEventListener('click', function(){
     window.location.href = "https://www.baruchceo.com";
 })
+
+document.querySelector("#ceo-slack").addEventListener('click', function(){
+    window.location.href = "https://join.slack.com/t/baruchceo/shared_invite/enQtNDAyNzMzMzAzNzk4LTczYmZlNDRiZWFjZmRlYTRjYjFiZTA5NTNlZGIwZTY2OWEzODIyMzAxYWU0N2Q0YWYyZjM3NTc3MGJhYzYyNTE";
+})
